5001efa69fdc5ab04282110268ff16f2f4c2a848
pvramani <pvramani@PVRAMANI-LAP.oradev.oraclecorp.com>
2021-08-27 18:01:28 +0530

    updating goal page object file

--- Project1/src/test/java/PageObjectsAndBehaviours/GoalPage.java
+++ Project1/src/test/java/PageObjectsAndBehaviours/GoalPage.java
@@ -1,13 +1,19 @@
 package PageObjectsAndBehaviours;
 
 import Utilities.TestCase;
+import Utilities.Status;
+import Utilities.Report;
 
 public class GoalPage extends TestCase {
 	
 	
 	public void Login() {
-		
+		Report.log("Step 1: logginng In", Status.BUSINESSSTEP);
+		Report.log("logginng In", Status.PASS);
+		Report.log("Step 3: logginng In", Status.BUSINESSSTEP);
+		Report.log("Step 4: logginng In", Status.BUSINESSSTEP);
 		System.out.println("In Login Method"+super.TestData.get("UserName"));
+		
 	}
 	public void Navigate() {
 		System.out.println("In Navigate Method");
